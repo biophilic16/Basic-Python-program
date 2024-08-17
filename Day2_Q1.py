344500d25d4b7dc4f31e7b7309fdc1af32963e17
DINESH KRISHNA S <163448293+biophilic16@users.noreply.github.com>
2024-08-17 11:57:30 +0530

    create variables for storing a person's name, age, and average test score.

---
+++ Day2_Q1.py
@@ -0,0 +1,5 @@
+#To create variables for storing a person's name, age, and average test score.
+name=input("Enter the name:")
+age=int(input("Enter the age:"))
+test_score=int(input("Enter the score:"))
+print(f"Name:{name}\nAge:{age}\nTest score:{test_score}")
